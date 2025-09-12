2d5961c6b58a5e710092a3ee817f613a601fc047
johnnyZeppelin <75575917+johnnyZeppelin@users.noreply.github.com>
2025-09-12 04:40:24 +0000

    Deploying to gh-pages from @ johnnyZeppelin/johnnyzeppelin.github.io@7771334847eb489a6c645387f83e961422d61c51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,8 +435,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-ulysses",
+          title: 'Ulysses',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
